27b07a7fc4ce46de5866f91fe4c096b006fa9de9
Kevin Scott <Kevin.Scott@publix.com>
2021-05-10 10:59:26 -0400

    db: Add product table to database project

---
+++ src/Domain/AIGProject.Domain/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AIGProject.Domain.Entities
+{
+    public class Product
+    {
+    }
+}
---
+++ src/Domain/Database/AIGProject.Database/dbo/Tables/Product.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[PRODUCT_ID]        INT           IDENTITY (1, 1)   NOT NULL,
+    [SKU]               VARCHAR(50)                         NULL, 
+    [TITLE]             VARCHAR(MAX)                        NULL, 
+    [DESCRIPTION]       VARCHAR(MAX)                        NULL, 
+    [PRICE]             DECIMAL(15, 2)                      NULL, 
+    [COST]              DECIMAL(15, 2)                      NULL,
+    PRIMARY KEY CLUSTERED ([PRODUCT_ID] ASC)
+)
