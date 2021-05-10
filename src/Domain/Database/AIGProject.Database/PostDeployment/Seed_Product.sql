f6fe1d5511f7a3b32694ac83dc68b9d8ed539f1b
Kevin Scott <Kevin.Scott@publix.com>
2021-05-10 11:13:30 -0400

    db: Add postdeployment seeding of product table allowing seeding of both local and azure database

---
+++ src/Domain/Database/AIGProject.Database/PostDeployment/Seed_Product.sql
@@ -0,0 +1,7 @@
+﻿SET NOCOUNT ON
+	--Delete current values
+DELETE FROM PRODUCT
+    --Seed Product values      
+INSERT INTO PRODUCT ( SKU, TITLE, DESCRIPTION, PRICE, COST) VALUES ('10-1-100','Great Book Part I','The first book in the series',9.99,4.27)
+INSERT INTO PRODUCT ( SKU, TITLE, DESCRIPTION, PRICE, COST) VALUES ('10-1-101','Great Book Part II','The sequel',10.00,4.68)
+INSERT INTO PRODUCT ( SKU, TITLE, DESCRIPTION, PRICE, COST) VALUES ('10-2-100','Yet Another Book','Another book here',9.95,4.35)
